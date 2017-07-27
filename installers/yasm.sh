4d571c1dff73b37a3b74236ae27182a9e77b9781
Eric Hocking <xaoseric@users.noreply.github.com>
2017-07-27 18:24:23 -0400

    yasm installer fix

--- installers/yasm.sh
+++ installers/yasm.sh
@@ -3,6 +3,7 @@ yasmInstall()
 	if [ ! -d $yasm ]; then
 		echo "Installing yasm 1.3.0..."
 		curl -O http://www.tortall.net/projects/yasm/releases/yasm-1.3.0.tar.gz
+		tar -xvf yasm-1.3.0.tar.gz
 		cd $yasm
 		./configure --prefix="$BUILD_DIR" --bindir="$BUILD_DIR/bin"
 		make
