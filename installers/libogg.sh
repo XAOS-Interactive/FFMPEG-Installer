64e8f315cb871c1caa965853f0b2351b779bfbdc
Eric Hocking <xaoseric@users.noreply.github.com>
2017-07-27 18:30:40 -0400

    libogg installer fix

--- installers/libogg.sh
+++ installers/libogg.sh
@@ -2,12 +2,9 @@ oggInstall()
 {
 	if [ ! -d $libogg ]; then 
 		echo "Installing Ogg 1.3.2..."
-		curl -O http://downloads.xiph.org/releases/ogg/libogg-1.3.2.tar.gz
-		tar xzvf libogg-1.3.2.tar.gz
-		cd $libogg
-		./configure --prefix="$BUILD_DIR" --disable-shared
-		make
-		make install
+		curl -O http://downloads.xiph.org/releases/ogg/libogg-1.3.2.tar.gz && tar xzvf libogg-1.3.2.tar.gz
+		cd $libogg && ./configure --prefix="$BUILD_DIR" --disable-shared
+		make && make install
 		echo
 	fi
 }
