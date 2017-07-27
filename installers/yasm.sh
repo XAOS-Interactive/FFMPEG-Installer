d383ff847ff3fd1e8fb7dbe2d079accfa07b82de
Eric Hocking <xaoseric@users.noreply.github.com>
2017-07-27 18:00:35 -0400

    fix yasm installer

--- installers/yasm.sh
+++ installers/yasm.sh
@@ -4,7 +4,7 @@ yasmInstall()
 		echo "Installing yasm 1.3.0..."
 		curl -O http://www.tortall.net/projects/yasm/releases/yasm-1.3.0.tar.gz
 		cd $yasm
-		./configure --prefix="$BUILD_DIR/ffmpeg_build" --bindir="$BUILD_DIR/bin"
+		./configure --prefix="$BUILD_DIR" --bindir="$BUILD_DIR/bin"
 		make
 		make install
 	fi
