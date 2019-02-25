d8980cbff42356594b17cd90b492d09703e87950
Eric Hocking <xaoseric@users.noreply.github.com>
2019-02-25 14:41:11 -0500

    update libogg to use wget

--- installers/libogg.sh
+++ installers/libogg.sh
@@ -2,7 +2,7 @@ oggInstall()
 {
 	if [ ! -d $libogg ]; then 
 		echo "Installing Ogg 1.3.2..."
-		curl -O http://downloads.xiph.org/releases/ogg/libogg-1.3.2.tar.gz && tar xzvf libogg-1.3.2.tar.gz
+		wget http://downloads.xiph.org/releases/ogg/libogg-1.3.2.tar.gz && tar xzvf libogg-1.3.2.tar.gz
 		cd $libogg && ./configure --prefix="$BUILD_DIR" --disable-shared
 		make && make install
 		echo
