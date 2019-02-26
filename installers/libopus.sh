68f4d7cfaf0458e076ca3a242088d18f39dfea07
Eric Hocking <xaoseric@users.noreply.github.com>
2019-02-26 12:00:39 -0500

    update how libopus it built

--- installers/libopus.sh
+++ installers/libopus.sh
@@ -2,9 +2,10 @@ opusInstall()
 {
 	if [ ! -d $libopus ]; then
 		echo "Installing opus 1.1.5..."
-		curl -O https://archive.mozilla.org/pub/opus/opus-1.1.5.tar.gz
-		tar xzvf opus-1.1.5.tar.gz
+		curl -O -L https://archive.mozilla.org/pub/opus/opus-1.3.tar.gz
+		tar xzvf opus-1.3.tar.gz
 		cd $libopus
+		./configure --prefix="$BUILD_DIR" --disable-shared
 		make
 		make install
 	fi
